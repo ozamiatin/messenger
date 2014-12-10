aa6c3f1e56c1157153df2ca158ccfb9a028abe49
Ilya Pekelny <ipekelny@mirantis.com>
2014-12-10 14:02:21 +0200

    PEP8 fix and docstrings.

--- client/console.py
+++ client/console.py
@@ -4,22 +4,27 @@ Created on Nov 12, 2014
 @author: ozamiatin
 '''
 
-import sys, socket
+import socket
+import sys
+
 from common import cred
 
-def printUsage():
-    print 'list - list users to chat with.\n'
-    print 'help - show usage help (this info).\n'
-    print 'q - terminate application\n'
-    print '[nick]: <msg> - pass msg to specified user!\n'
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((cred.HOST, cred.PORT))
+def print_usage():
+    print '\n'.join('list - list users to chat with.',
+                    'help - show usage help (this info).',
+                    'q - terminate application',
+                    '[nick]: <msg> - pass msg to specified user!')
+
+
+STREAM_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+STREAM_SOCKET.connect((cred.HOST, cred.PORT))
+
 
 if __name__ == '__main__':
-    print 'Welcome to messenger!\n'    
-    
+    print 'Welcome to messenger!\n'
+
     while True:
         print 'msg$>'
         msg = sys.stdin.readline()
-        s.sendall(msg)
+        STREAM_SOCKET.sendall(msg)
--- client/manager.py
+++ client/manager.py
@@ -9,22 +9,27 @@ from client.comunicator import ZeroMqCommunicator
 from client.comunicator import ZeroMqUsersListUpdater
 
 class CommunicationManager(object):
-    
+    """Routing service."""
+
     def __init__(self):
         messaging_port = str(random.randint(5000, 5999))
-        self.communicator = ZeroMqCommunicator(messaging_port) 
+        self.communicator = ZeroMqCommunicator(messaging_port)
 
-    def sendHere(self, name):
+    def send_here(self, name):
+        """Route socket to the given socket."""
         self.userName = name
         msg = {"kind": "here",
                "name": name,
-               "address": self.communicator.getAddress()}
-        self.communicator.sendObject(msg)
-        rep = self.communicator.receiveString()
+               "address": self.communicator.get_address()}
+        self.communicator.send_object(msg)
+        rep = self.communicator.receive_string()
         if (rep != "registered"):
             raise Exception("Not registered!")
 
-    def runListUpdater(self, usersListView):
-        self.listUpdater = ZeroMqUsersListUpdater(usersListView,
+    # TODO: why do we need to start a handler here?
+    # Maybe better to send a signal to a handler.
+    def run_handler(self, users_list_view):
+        self.listUpdater = ZeroMqUsersListUpdater(users_list_view,
                                                   self.communicator.context)
-        self.listUpdater.start()
+        # FIXME: there is no such a member.
+        self.list_updater.start()
