2838fd0a08fe2ff4d023e210cd331f061e26eed1
Oleksii Zamiatin <ozamiatin@mirantis.com>
2015-01-14 11:56:08 +0200

    Updated running scripts

--- runclient.sh
+++ runclient.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-cd work/om-env
-. bin/activate
-
+. ~/work/om-env/bin/activate
 cd ~/workspace/messenger
 python client/console.py $1
+#python ~/work/client/console.py $1
+
+#cd ~/workspace/messenger
+#python client/console.py $1
 #python client/ui/ui.py
 #python client/omclient.py
 #python client/omlistener.py
 
 #python client/bench_cast.py
+
+#. ~/work/rmq-samples/runclient.sh
--- runserver.sh
+++ runserver.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-cd work/om-env
-. bin/activate
-
+. ~/work/om-env/bin/activate
 cd ~/workspace/messenger
-python server/console.py
+python server/console.py $1
 #python server/omserver.py
 #python server/omnotifier.py
 #python server/bench_cast_srv.py
